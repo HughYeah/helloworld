c1814db3f6be82c181432d704f521784a6662c64
HughYeah <414322700@qq.com>
2017-07-27 19:02:55 +0800

    part2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  private id:number = 2;
+  private type:number = 1;
+
+  users: Array<any> = [
+    {
+      "id":0,
+      "name":"Hugh",
+      "sex":"Male",
+      "mobile":"15900000000",
+      "email":"hugh@qq.com"
+    },
+      {
+      "id":1,
+      "name":"Hugh1",
+      "sex":"Male",
+      "mobile":"15900000002",
+      "email":"hugh@qq.com"
+    },
+    {
+      "id":2,
+      "name":"Hugh2",
+      "sex":"Male",
+      "mobile":"15900000003",
+      "email":"hugh@qq.com"
+    }
+  ]
+deleteLast(){
+  this.users.pop()
+}
+saveNewUser(){
+
+  this.users.push(   {
+      "id":this.id++,
+      "name":"Hugh"+this.id,
+      "sex":"Male",
+      "mobile":"15900000001",
+      "email":"hugh@qq.com"
+    })
+}
+   compareAsccending(a, b) {
+       return (a.id - b.id);
+  }
+  compareDesccending(a, b) {
+       return (b.id - a.id);
+  }
+  compareRandom(a, b) {
+       return (b.id * Math.random() - a.id * Math.random());
+  }
+
+  sortByAsccending() {
+    // 参考MDN Array操作的API文档 Array相关方法方法
+    this.users.sort(this.compareAsccending);
+  }
+  sortByDesccending() {
+    // 参考MDN Array操作的API文档 Array相关方法
+    // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array
+    this.users.sort(this.compareDesccending);
+  }
+  sortByRadom() {
+    // 参考MDN Array操作的API文档 Math相关方法
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+    this.users.sort(this.compareRandom);
+  }
 }
